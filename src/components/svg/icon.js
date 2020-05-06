3a04b8eb97bd8aeca7cfb1a5ee9c35051d4eb962
nhi101298 <nhiphan.24hdev@gmail.com>
2020-05-06 20:05:34 +0700

    add svg icon

---
+++ src/components/svg/icon.js
@@ -0,0 +1,145 @@
+import React from "react";
+
+export const dashboard = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M12,3L2,12H5V20H19V12H22M9,18H7V12H9M13,18H11V10H13M17,18H15V14H17"
+    />
+  </svg>
+);
+
+export const user = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M21.1,12.5L22.5,13.91L15.97,20.5L12.5,17L13.9,15.59L15.97,17.67L21.1,12.5M10,17L13,20H3V18C3,15.79 6.58,14 11,14L12.89,14.11L10,17M11,4A4,4 0 0,1 15,8A4,4 0 0,1 11,12A4,4 0 0,1 7,8A4,4 0 0,1 11,4Z"
+    />
+  </svg>
+);
+
+export const book = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M9 3V18H12V3H9M12 5L16 18L19 17L15 4L12 5M5 5V18H8V5H5M3 19V21H21V19H3Z"
+    />
+  </svg>
+);
+
+export const type = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M5,9.5L7.5,14H2.5L5,9.5M3,4H7V8H3V4M5,20A2,2 0 0,0 7,18A2,2 0 0,0 5,16A2,2 0 0,0 3,18A2,2 0 0,0 5,20M9,5V7H21V5H9M9,19H21V17H9V19M9,13H21V11H9V13Z"
+    />
+  </svg>
+);
+
+export const coupon = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M4,4A2,2 0 0,0 2,6V10C3.11,10 4,10.9 4,12A2,2 0 0,1 2,14V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V14A2,2 0 0,1 20,12C20,10.89 20.9,10 22,10V6C22,4.89 21.1,4 20,4H4M15.5,7L17,8.5L8.5,17L7,15.5L15.5,7M8.81,7.04C9.79,7.04 10.58,7.83 10.58,8.81A1.77,1.77 0 0,1 8.81,10.58C7.83,10.58 7.04,9.79 7.04,8.81A1.77,1.77 0 0,1 8.81,7.04M15.19,13.42C16.17,13.42 16.96,14.21 16.96,15.19A1.77,1.77 0 0,1 15.19,16.96C14.21,16.96 13.42,16.17 13.42,15.19A1.77,1.77 0 0,1 15.19,13.42Z"
+    />
+  </svg>
+);
+
+export const invoice = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M3,22L4.5,20.5L6,22L7.5,20.5L9,22L10.5,20.5L12,22L13.5,20.5L15,22L16.5,20.5L18,22L19.5,20.5L21,22V2L19.5,3.5L18,2L16.5,3.5L15,2L13.5,3.5L12,2L10.5,3.5L9,2L7.5,3.5L6,2L4.5,3.5L3,2M18,9H6V7H18M18,13H6V11H18M18,17H6V15H18V17Z"
+    />
+  </svg>
+);
+
+export const review = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M18,14H10.5L12.5,12H18M6,14V11.5L12.88,4.64C13.07,4.45 13.39,4.45 13.59,4.64L15.35,6.41C15.55,6.61 15.55,6.92 15.35,7.12L8.47,14M20,2H4A2,2 0 0,0 2,4V22L6,18H20A2,2 0 0,0 22,16V4C22,2.89 21.1,2 20,2Z"
+    />
+  </svg>
+);
+
+export const reply = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M2 12C2 17.5 6.5 22 12 22S22 17.5 22 12 17.5 2 12 2 2 6.5 2 12M5 11L10 6V9C15.06 9.73 17.28 13.33 18 17C16.19 14.43 13.61 13 10 13V16L5 11Z"
+    />
+  </svg>
+);
+
+export const shipping = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M18 18.5C18.83 18.5 19.5 17.83 19.5 17C19.5 16.17 18.83 15.5 18 15.5C17.17 15.5 16.5 16.17 16.5 17C16.5 17.83 17.17 18.5 18 18.5M19.5 9.5H17V12H21.46L19.5 9.5M6 18.5C6.83 18.5 7.5 17.83 7.5 17C7.5 16.17 6.83 15.5 6 15.5C5.17 15.5 4.5 16.17 4.5 17C4.5 17.83 5.17 18.5 6 18.5M20 8L23 12V17H21C21 18.66 19.66 20 18 20C16.34 20 15 18.66 15 17H9C9 18.66 7.66 20 6 20C4.34 20 3 18.66 3 17H1V6C1 4.89 1.89 4 3 4H17V8H20M3 6V15H3.76C4.31 14.39 5.11 14 6 14C6.89 14 7.69 14.39 8.24 15H15V6H3M10 7L13.5 10.5L10 14V11.5H5V9.5H10V7Z"
+    />
+  </svg>
+);
+
+export const subscribe = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M23.5,17L18.5,22L15,18.5L16.5,17L18.5,19L22,15.5L23.5,17M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,17C12.5,17 12.97,16.93 13.42,16.79C13.15,17.5 13,18.22 13,19V19.45L12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5C17,4.5 21.27,7.61 23,12C22.75,12.64 22.44,13.26 22.08,13.85C21.18,13.31 20.12,13 19,13C18.22,13 17.5,13.15 16.79,13.42C16.93,12.97 17,12.5 17,12A5,5 0 0,0 12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17Z"
+    />
+  </svg>
+);
+
+export const profile = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z"
+    />
+  </svg>
+);
+
+export const editProfile = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z"
+    />
+  </svg>
+);
+
+export const logout = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 22">
+    <path
+      fill="currentColor"
+      d="M16,17V14H9V10H16V7L21,12L16,17M14,2A2,2 0 0,1 16,4V6H14V4H5V20H14V18H16V20A2,2 0 0,1 14,22H5A2,2 0 0,1 3,20V4A2,2 0 0,1 5,2H14Z"
+    />
+  </svg>
+);
+
+export const add = (
+  <svg style={{ width: "16px", height: "16px" }} viewBox="0 0 20 24">
+    <path
+      fill="currentColor"
+      d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
+    />
+  </svg>
+);
+
+export const edit = (
+  <svg style={{ width: "20px", height: "20px" }} viewBox="0 0 20 24">
+    <path
+      fill="#fcaa38"
+      d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+    />
+  </svg>
+);
+
+export const del = (
+  <svg style={{ width: "20px", height: "20px" }} viewBox="0 0 20 24">
+    <path
+      fill="#d93b38"
+      d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
+    />
+  </svg>
+);
