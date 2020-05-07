fc96af2eb6b5cf98730f82bce5def84e193e3737
nhi101298 <nhiphan.24hdev@gmail.com>
2020-05-08 01:31:14 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-import Login from "./containers/Login/Login";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 import "./assets/css/style.css";
-import SideBar from "./containers/Layout/components/SideBar";
-import TopBar from "./containers/Layout/components/TopBar";
+
+import Login from "./containers/Login/Login";
 import Layout from "./containers/Layout/Layout";
+import Book from "./containers/Book/Book";
 
 function App() {
-  return (
-    <Layout>
-      <p>abc</p>
-    </Layout>
-  );
+  const storedUser = localStorage.getItem("user");
+  const [user, setUser] = useState(storedUser);
+
+  const handleLog = (user) => {
+    setUser(user);
+  };
+
+  if (localStorage.getItem("user")) {
+    return (
+      <Router>
+        <Layout onLogout={handleLog}>
+          <Route path="/book" component={Book} />
+          <Redirect exact from="/" to="/book" />
+        </Layout>
+      </Router>
+    );
+  } else {
+    return (
+      <Router>
+        <Route
+          exact
+          path="/login"
+          render={() => <Login onLogin={handleLog} />}
+        />
+        <Redirect exact from="/" to="/login" />
+      </Router>
+    );
+  }
 }
 
 export default App;
