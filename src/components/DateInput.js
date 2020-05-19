d48c7fe08e0c24311f7e52e1aae104e6b473cad9
nhi101298 <nhiphan.24hdev@gmail.com>
2020-05-19 10:20:40 +0700

    add DateInput component

---
+++ src/components/DateInput.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const DateInput = ({ field, form, ...props }) => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  return (
+    <div className="form-group">
+      <label>{props.label}</label>
+      <DatePicker
+        name={field.name}
+        className="form-control"
+        dateFormat="dd/MM/yyyy"
+        selected={startDate}
+        onChange={(date) => {
+          setStartDate(date);
+          form.setFieldValue(field.name, date);
+        }}
+      />
+    </div>
+  );
+};
+
+export default DateInput;
