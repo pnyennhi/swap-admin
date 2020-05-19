845c437c3eff1d9bc11061d4c82c8ada53ed103d
nhi101298 <nhiphan.24hdev@gmail.com>
2020-05-19 10:20:25 +0700

    add NumberInput component

---
+++ src/components/NumberInput.js
@@ -0,0 +1,25 @@
+import NumberFormat from "react-number-format";
+import React from "react";
+
+const NumberInput = ({ field, form, ...props }) => {
+  return (
+    <div className="form-group">
+      <label>{props.label}</label>
+      <NumberFormat
+        name={field.name}
+        thousandSeparator="."
+        decimalSeparator=","
+        allowNegative={false}
+        onValueChange={(val) =>
+          form.setFieldValue(field.name, val.floatValue || "")
+        }
+        {...props}
+      />
+      {form.errors[field.name] && form.touched[field.name] ? (
+        <div className="input-feedback">{form.errors[field.name]}</div>
+      ) : null}
+    </div>
+  );
+};
+
+export default NumberInput;
