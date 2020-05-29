dd42f2ac9b468222bb66908372fe5f48093ae3f7
nhi101298 <nhiphan.24hdev@gmail.com>
2020-05-29 12:15:28 +0700

    add pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+const Pagination = (props) => {
+  const { pageTotal, currentPage, onChange } = props;
+
+  let pages = [];
+
+  for (let i = 1; i <= pageTotal; i++) pages.push(i);
+
+  return (
+    <div className="row">
+      <div class="col-sm-12 col-md-7">
+        <div
+          class="dataTables_paginate paging_simple_numbers"
+          id="dataTableExample_paginate"
+        >
+          <ul class="pagination">
+            <li
+              class={
+                currentPage === 1
+                  ? "paginate_button page-item previous disabled"
+                  : "paginate_button page-item previous"
+              }
+              id="dataTableExample_previous"
+            >
+              <a
+                class="page-link"
+                onClick={() => {
+                  onChange(currentPage - 1);
+                }}
+              >
+                Previous
+              </a>
+            </li>
+            {pages.map((page) => (
+              <li
+                class={
+                  page === currentPage
+                    ? "paginate_button page-item active"
+                    : "paginate_button page-item"
+                }
+                id="dataTableExample_next"
+              >
+                <a
+                  class="page-link"
+                  onClick={() => {
+                    onChange(page);
+                  }}
+                >
+                  {page}
+                </a>
+              </li>
+            ))}
+            <li
+              class={
+                currentPage === pageTotal
+                  ? "paginate_button page-item next disabled"
+                  : "paginate_button page-item next"
+              }
+              id="dataTableExample_next"
+            >
+              <a
+                class="page-link"
+                onClick={() => {
+                  onChange(currentPage + 1);
+                }}
+              >
+                Next
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
