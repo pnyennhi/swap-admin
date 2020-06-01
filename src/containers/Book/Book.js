5ff63d332d53fc8b1a5e8e4f90504e9f3df9504c
nhi101298 <nhiphan.24hdev@gmail.com>
2020-06-01 21:07:31 +0700

    add search handle, sort handle, pagination

--- src/containers/Book/Book.js
+++ src/containers/Book/Book.js
@@ -4,15 +4,30 @@ import BookTable from "./components/BookTable";
 import { books as fakeBooks } from "../../FakeData";
 import DeleteBookModal from "./components/DeleteBookModal";
 import AddBookModal from "./components/AddBookModal";
+import Pagination from "../../components/Pagination";
+import axios from "axios";
 
 const Book = () => {
   const [books, setBooks] = useState(fakeBooks);
   const [selectedBooks, setSelectedBooks] = useState([]);
   const [deletedBook, setDeletedBook] = useState(null);
-  const [searchedBooks, setSearchedBooks] = useState([]);
   const [showDeleteModal, setShowDeletedModal] = useState(false);
   const [showAddModal, setShowAddModal] = useState(false);
-  const [isSearching, setIsSearching] = useState(false);
+  const [sortConfig, setSortConfig] = useState(null);
+  const [totalRows, setTotalRows] = useState(21);
+  const [filters, setFilters] = useState({
+    currentPage: 1,
+    limit: 10,
+    search: null,
+  });
+
+  useEffect(() => {
+    //call API get when mounted (category list, publisher list)
+  }, []);
+
+  useEffect(() => {
+    //call API get when filters change (get book list)
+  }, [filters]);
 
   const handleSetDeletedBook = (id) => {
     setDeletedBook(id);
@@ -32,19 +47,19 @@ const Book = () => {
     //id is an array
     setDeletedBook(null);
     setShowDeletedModal(false);
-    handleDeleteBooks(ids);
+    // handleDeleteBooks(ids);
   };
 
-  const handleDeleteBooks = (delIds) => {
-    const newBooks = books.filter((book) => delIds.indexOf(book.id) === -1);
-    setBooks(newBooks);
-    const newSelectedBooks = selectedBooks.filter(
-      (bookId) => delIds.indexOf(bookId) === -1
-    );
-    console.log(newSelectedBooks);
+  // const handleDeleteBooks = (delIds) => {
+  //   const newBooks = books.filter((book) => delIds.indexOf(book.id) === -1);
+  //   setBooks(newBooks);
+  //   const newSelectedBooks = selectedBooks.filter(
+  //     (bookId) => delIds.indexOf(bookId) === -1
+  //   );
+  //   console.log(newSelectedBooks);
 
-    setSelectedBooks(newSelectedBooks);
-  };
+  //   setSelectedBooks(newSelectedBooks);
+  // };
 
   const handleSelectOneBook = (e, id) => {
     const selectedIndex = selectedBooks.indexOf(id);
@@ -56,27 +71,52 @@ const Book = () => {
     } else {
       newSelectedBooks.splice(selectedIndex, 1);
     }
-
     setSelectedBooks(newSelectedBooks);
   };
 
   const handleSelectAll = (e) => {
-    if (e.target.checked) setSelectedBooks(books.map((book) => book.id));
+    if (e.target.checked) setSelectedBooks(books.map((book) => book.BookID));
     else setSelectedBooks([]);
   };
 
-  const handleSearchBooks = (e) => {
-    var key = e.target.value;
-    var results = books.filter(
-      (book) =>
-        JSON.stringify(book).toLowerCase().search(key.toLowerCase()) > -1
-    );
-    console.log(results);
-    if (key) setIsSearching(true);
-    else setIsSearching(false);
+  const handleSearchBooks = () => {
+    // var key = e.target.value;
+    // var results = books.filter(
+    //   (book) =>
+    //     JSON.stringify(book).toLowerCase().search(key.toLowerCase()) > -1
+    // );
+    // console.log(results);
+    // if (key) setIsSearching(true);
+    // else setIsSearching(false);
+    // setSearchedBooks(results);
 
-    setSearchedBooks(results);
+    //call API search
+    alert(filters.search);
   };
+
+  const handleChangePage = (page) => {
+    setFilters({ ...filters, currentPage: page });
+  };
+
+  const handleSort = (field) => {
+    if (!sortConfig) setSortConfig({ field: field, order: "asc" });
+    else if (sortConfig.field !== field)
+      setSortConfig({ field: field, order: "asc" });
+    else if (sortConfig.order === "asc")
+      setSortConfig({
+        field: field,
+        order: "desc",
+      });
+    else
+      setSortConfig({
+        field: field,
+        order: "asc",
+      });
+  };
+
+  useEffect(() => {
+    if (sortConfig) alert(JSON.stringify(sortConfig));
+  }, [sortConfig]);
 
   return (
     <>
@@ -90,16 +130,30 @@ const Book = () => {
             <div class="card-body">
               <div class="row align-items-md-center justify-content-between mb-4">
                 <div class="col-sm-12 col-md-8">
-                  <div id="dataTableExample_filter" class="dataTables_filter">
-                    <input
-                      class="form-control"
-                      placeholder="Search"
-                      onChange={(e) => {
-                        handleSearchBooks(e);
-                      }}
-                    />
+                  <div className="row align-items-md-center">
+                    <div class="col-sm-9 col-md-8  dataTables_filter">
+                      <input
+                        class="form-control"
+                        placeholder="Search"
+                        onChange={(e) => {
+                          setFilters({ ...filters, search: e.target.value });
+                        }}
+                        onKeyUp={(e) => {
+                          if (e.keyCode === 13) handleSearchBooks();
+                        }}
+                      />
+                    </div>
+                    <div class="col-sm-3 col-md-4">
+                      <a
+                        className="btn btn-primary mr-2 mb-2 mb-md-0 text-white"
+                        onClick={() => handleSearchBooks()}
+                      >
+                        Tìm
+                      </a>
+                    </div>
                   </div>
                 </div>
+
                 {selectedBooks.length > 0 && (
                   <div class="col-sm-12 col-md-2 text-right">
                     <a
@@ -127,15 +181,25 @@ const Book = () => {
               </div>
 
               <BookTable
-                books={
-                  searchedBooks.length > 0 || isSearching
-                    ? searchedBooks
-                    : books
-                }
+                // books={
+                //   searchedBooks.length > 0 || isSearching
+                //     ? searchedBooks
+                //     : books
+                // }
+
+                books={books}
                 selectedBooks={selectedBooks}
                 onDelete={handleSetDeletedBook}
                 onSelect={handleSelectOneBook}
                 onSelectAll={handleSelectAll}
+                onSort={handleSort}
+              />
+
+              <Pagination
+                totalRows={totalRows}
+                currentPage={filters.currentPage}
+                limit={filters.limit}
+                onChange={handleChangePage}
               />
 
               {(Boolean(deletedBook) || showDeleteModal) && (
