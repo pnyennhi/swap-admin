3ac56eb01e8bf6d8f9e36d427a7028548ba3abeb
nhi101298 <nhiphan.24hdev@gmail.com>
2020-06-23 01:27:52 +0700

    fix interceptor in axios

--- src/Instance.js
+++ src/Instance.js
@@ -1,16 +1,31 @@
-import Axios from "axios";
+import axios from "axios";
 
-const Instance = () =>
-  Axios.create({
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem("TOKEN_AUTH"),
-    },
-    // validateStatus: function (status) {
-    //   if (status === 401) {
-    //     localStorage.clear();
-    //     window.location.assign("/");
-    //   }
-    //   return status;
-    // },
-  });
+const Instance = axios.create();
+
+Instance.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization =
+      "Bearer " + localStorage.getItem("TOKEN_AUTH");
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+Instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      //  router.push('/login');
+      alert(1);
+      return Promise.reject(error);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default Instance;
