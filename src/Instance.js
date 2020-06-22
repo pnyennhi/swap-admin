66138859daa19298a1d2e7ee4b6785f3ad94fb62
nhi101298 <nhiphan.24hdev@gmail.com>
2020-06-22 10:12:59 +0700

    remove validateStatus in axios

--- src/Instance.js
+++ src/Instance.js
@@ -5,12 +5,12 @@ const Instance = () =>
     headers: {
       Authorization: "Bearer " + localStorage.getItem("TOKEN_AUTH"),
     },
-    validateStatus: function (status) {
-      if (status === 401) {
-        localStorage.clear();
-        window.location.assign("/");
-      }
-      return status;
-    },
+    // validateStatus: function (status) {
+    //   if (status === 401) {
+    //     localStorage.clear();
+    //     window.location.assign("/");
+    //   }
+    //   return status;
+    // },
   });
 export default Instance;
