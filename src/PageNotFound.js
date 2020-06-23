c97c67afe47ff51531dc8d082baafc154af2809c
nhi101298 <nhiphan.24hdev@gmail.com>
2020-06-23 13:15:26 +0700

    add page not found

---
+++ src/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="notfound">
+      <h1 className="notfound-title">404</h1>
+      <h3 className="notfound-content">OOPS!</h3>
+      <h3 className="notfound-content">
+        This page doesn't exist or is unavailable
+      </h3>
+    </div>
+  );
+};
+
+export default PageNotFound;
