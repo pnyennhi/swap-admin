52c7ef2e4085d0ac8664793f3efe4d987f17124b
nhi101298 <nhiphan.24hdev@gmail.com>
2020-06-25 12:07:40 +0700

    change book modal UI

--- src/containers/Book/components/BookDetailModal.js
+++ src/containers/Book/components/BookDetailModal.js
@@ -34,7 +34,7 @@ const BookDetailModal = (props) => {
   }, []);
 
   return (
-    <Modal show={show}>
+    <Modal show={show} maxWidth="75%">
       <div className="modal-header">
         <h5 className="modal-title" id="exampleModalLabel">
           Chi tiết sách
@@ -53,120 +53,141 @@ const BookDetailModal = (props) => {
         <>
           <div className="modal-body">
             <div className="form-detail">
-              <div className="form-group">
-                <label>
-                  <b>Id</b>
-                </label>
-                <p>{book.bookID}</p>
-              </div>
+              <div className="row">
+                <div className="col-sm-12 col-md-6">
+                  <div className="form-group">
+                    <label>
+                      <b>Id</b>
+                    </label>
+                    <p>{book.bookID}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Tên sách</b>
-                </label>
-                <p>{book.nameBook}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Tên sách</b>
+                    </label>
+                    <p>{book.nameBook}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Tác giả</b>
-                </label>
-                <p>{book.author}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Tác giả</b>
+                    </label>
+                    <p>{book.author}</p>
+                  </div>
 
-              {/* <div className="form-group">
-              <label>
-                <b>Thể loại</b>
-              </label>
-              <p>
-                {
-                  categories.find(
-                    (category) => category.categoryID === book.categoryID
-                  ).category
-                }
-              </p>
-            </div> */}
+                  <div className="form-group">
+                    <label>
+                      <b>Thể loại</b>
+                    </label>
+                    <p>
+                      {
+                        categories.find(
+                          (category) => category.categoryID === book.categoryID
+                        )?.category
+                      }
+                    </p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Giá gốc</b>
-                </label>
-                <p>{book.originalPrice}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Nhà xuất bản</b>
+                    </label>
+                    <p>
+                      {
+                        publishers.find(
+                          (publisher) =>
+                            publisher.publisherID === book.publisherID
+                        )?.publisher
+                      }
+                    </p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Giá bán</b>
-                </label>
-                <p>{book.price}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Giá gốc</b>
+                    </label>
+                    <p>{book.originalPrice}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Ảnh bìa</b>
-                </label>
-                <br />
-                <img
-                  src={book.imageLink}
-                  alt={book.nameBook}
-                  className="preview-image"
-                />
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Giá bán</b>
+                    </label>
+                    <p>{book.price}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Loại</b>
-                </label>
-                <p>{book.format}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Kích thước</b>
+                    </label>
+                    <p>{book.dimensions}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Kích thước</b>
-                </label>
-                <p>{book.dimensions}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Khối lượng</b>
+                    </label>
+                    <p>{book.weight}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Khối lượng</b>
-                </label>
-                <p>{book.weight}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Số trang</b>
+                    </label>
+                    <p>{book.numberOfPage}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Số trang</b>
-                </label>
-                <p>{book.numberOfPage}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Số lượng nhập</b>
+                    </label>
+                    <p>{book.quantityIn}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Thông tin</b>
-                </label>
-                <p>{book.information}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Số lượng bán</b>
+                    </label>
+                    <p>{book.quantityOut}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Số lượng nhập</b>
-                </label>
-                <p>{book.quantityIn}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Ngày nhập</b>
+                    </label>
+                    <p>{new Date(book.date).toLocaleDateString("en-GB")}</p>
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <div className="form-group">
+                    <label>
+                      <b>Ảnh bìa</b>
+                    </label>
+                    <br />
+                    <img
+                      src={book.imageLink}
+                      alt={book.nameBook}
+                      className="preview-image"
+                    />
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Số lượng bán</b>
-                </label>
-                <p>{book.quantityOut}</p>
-              </div>
+                  <div className="form-group">
+                    <label>
+                      <b>Loại</b>
+                    </label>
+                    <p>{book.format}</p>
+                  </div>
 
-              <div className="form-group">
-                <label>
-                  <b>Ngày nhập</b>
-                </label>
-                <p>{new Date(book.date).toLocaleDateString("en-GB")}</p>
+                  <div className="form-group">
+                    <label>
+                      <b>Thông tin</b>
+                    </label>
+                    <div style={{ height: "300px", overflowY: "auto" }}>
+                      {book.information}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
