1288460d31ee1ca5aed4bb575d006f73937accf8
nhi101298 <nhiphan.24hdev@gmail.com>
2020-06-25 11:47:29 +0700

    add maxWidth to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,7 +6,10 @@ const Modal = (props) => {
       className={props.show ? "modal fade show" : "modal fade"}
       style={{ display: props.show ? "block" : "none" }}
     >
-      <div className="modal-dialog" role="document">
+      <div
+        className="modal-dialog"
+        style={{ maxWidth: props.maxWidth ? props.maxWidth : "500px" }}
+      >
         <div className="modal-content">{props.children}</div>
       </div>
     </div>
