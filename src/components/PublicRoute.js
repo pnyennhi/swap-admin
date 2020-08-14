5d17c6a20dd9d2a662b79e3255576f645e031fd7
nhi101298 <nhiphan.24hdev@gmail.com>
2020-08-14 08:44:53 +0700

    add in PublicRoute

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+//abc
 const PublicRoute = ({ component: Component, restricted, ...rest }) => {
   const user = useSelector((store) => store.user);
 
